071f311584d2afa480c5bdabe152f4ccccec6974
Nizar Fergag <fergag99@gmail.com>
2022-04-06 18:24:15 +0200

    struttura grafica

--- asset/js/main.js
+++ asset/js/main.js
@@ -2,7 +2,16 @@ var app = new Vue(
     {
         el: '#root',
         data: {
-           
+            profilo: {
+                nome: 'Sofia',
+                img: 'avatar_io'
+            },
+            utentiChat: [
+                {
+                  nome: 'Michele',
+                  img: 'avatar_1'  
+                },
+            ]
         }
     }
 )
